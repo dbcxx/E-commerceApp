5b5812176abc59f6835a26192feb1cc2b4853f97
dolapo <bamideledolapo.s@gmail.com>
2022-03-08 00:57:52 +0100

    Add project files.

---
+++ E-commerceApp/Models/ProductTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceApp.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductType { get; set; }
+    }
+}
